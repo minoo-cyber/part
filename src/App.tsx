5b7b57c929042250ddb9f974beac20d16b235f76
maryam binesh <maryambinesh@maryams-MacBook-Air.local>
2023-10-15 08:46:08 +0330

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -11,32 +11,36 @@ import InvoiceAdd from "./pages/invoicesAdd";
 import InvoicesSearch from "./pages/invoicesSearch";
 import useAppSelector from "./hooks/useSelector";
 import Otp from "./pages/otp";
+import { useEffect, useLayoutEffect, useState } from "react";
 
 const queryClient = new QueryClient();
 
 const App = () => {
   const { token } = useAppSelector((state) => state.user);
+  const [auth, setAuth] = useState(true);
+
   let selectedTheme = lightTheme;
+  useEffect(() => {
+    if (token != "") {
+      setAuth(true);
+    } else {
+      setAuth(false);
+    }
+  }, [auth, token]);
+
   return (
     <ThemeProvider theme={selectedTheme}>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <Routes>
-            {token ? (
-              <>
-                <Route path="/" element={<Panel />} />
-                <Route path="/user" element={<User />} />
-                <Route path="/invoiceAdd" element={<InvoiceAdd />} />
-                <Route path="/invoicesSearch" element={<InvoicesSearch />} />
-                <Route path="/pending" element={<PendingQueris />} />
-                <Route path="/newItem" element={<AddNewItem />} />
-                <Route path="/otp" element={<Otp />} />
-              </>
-            ) : (
-              <>
-                <Route path="/login" element={<Login />} />
-              </>
-            )}
+            <Route path="/" element={<Panel />} />
+            <Route path="/user" element={<User />} />
+            <Route path="/invoiceAdd" element={<InvoiceAdd />} />
+            <Route path="/invoicesSearch" element={<InvoicesSearch />} />
+            <Route path="/pending" element={<PendingQueris />} />
+            <Route path="/newItem" element={<AddNewItem />} />
+            <Route path="/otp" element={<Otp />} />
+            {!auth && <Route path="/login" element={<Login />} />}
           </Routes>
         </BrowserRouter>
       </QueryClientProvider>
